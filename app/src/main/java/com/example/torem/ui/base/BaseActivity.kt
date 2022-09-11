151e04c156b7e5e503d48a248d200317cbec1845
ucok <ulubis98@gmail.com>
2022-09-11 08:25:09 +0700

    create activity base class

---
+++ app/src/main/java/com/example/torem/ui/base/BaseActivity.kt
@@ -0,0 +1,23 @@
+package com.example.torem.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+
+/**
+ * Abstract Activity which binds [ViewModel] [VM] and [ViewBinding] [VB]
+ */
+abstract class BaseActivity<VM : ViewModel, VB : ViewBinding> : AppCompatActivity() {
+    protected abstract val mViewModel : VM
+
+    protected lateinit var mViewBinding: VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mViewBinding = getViewBinding()
+    }
+
+    abstract fun getViewBinding(): VB
+}
